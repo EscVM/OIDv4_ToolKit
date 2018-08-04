a8a650cc0b3aad530ae9e93cc3cfd76dd0106f6e
Angelo Tartaglia <37141579+keldrom@users.noreply.github.com>
2018-08-04 18:29:50 +0200

    Added class with space

--- modules/downloader.py
+++ modules/downloader.py
@@ -50,7 +50,7 @@ def download_img(folder, dataset_dir, class_name, images_list):
     if len(images_list) > 0:
         print("[INFO] Download of {} images in {}.".format(len(images_list), folder))
         for image in tqdm(images_list):
-            path = image_dir + '/' + str(image) + '.jpg ' + download_dir
+            path = image_dir + '/' + str(image) + '.jpg ' + '"' + download_dir + '"'
             command = 'aws s3 --no-sign-request --only-show-errors cp s3://open-images-dataset/' + path
             os.system(command)
 
