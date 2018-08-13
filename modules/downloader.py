85f02c7774270095fd0ebfc135721901d5908955
Denis Zuenko <neron.v2@gmail.com>
2018-08-14 00:29:47 +0300

    Update downloader.py
    Change entirely get_label function:
    - From 40 hours to 2 minutes (without pools)
    - New label file format

--- modules/downloader.py
+++ modules/downloader.py
@@ -1,8 +1,8 @@
 import os
-import cv2
 from tqdm import tqdm
 from modules.utils import images_options
-from multiprocessing.dummy import Pool as ThreadPool 
+from multiprocessing.dummy import Pool as ThreadPool
+
 
 def download(args, df_val, folder, dataset_dir, class_name, class_code, class_list=None, threads = 4):
     '''
@@ -22,7 +22,7 @@ def download(args, df_val, folder, dataset_dir, class_name, class_code, class_li
     print('[INFO] Downloading {} images.'.format(args.type_csv))
     df_val_images = images_options(df_val, args)
 
-    images_list = df_val_images['ImageID'][df_val_images.LabelName == class_code].values
+    images_list = df_val_images['ImageID'][df_val_images['LabelName'] == class_code].values
     images_list = set(images_list)
     if class_list is not None:
         class_name_list = '_'.join(class_list)
@@ -50,9 +50,8 @@ def download_img(folder, dataset_dir, class_name, images_list, threads):
     downloaded_images_list = [f.split('.')[0] for f in os.listdir(download_dir)]
     images_list = list(set(images_list) - set(downloaded_images_list))
 
-    pool = ThreadPool(threads)
-    
     if len(images_list) > 0:
+        pool = ThreadPool(threads)
         print("[INFO] Download of {} images in {}.".format(len(images_list), folder))
         commands = []
         for image in images_list:
@@ -61,17 +60,18 @@ def download_img(folder, dataset_dir, class_name, images_list, threads):
             commands.append(command)
         
         list(tqdm(pool.imap(os.system, commands), total = len(commands) ))
-
         print('[INFO] Done!')
-        pool.close() 
+        
+        pool.close()
         pool.join() 
     else:
         print('[INFO] All images already downloaded.')
 
+    
 
 def get_label(folder, dataset_dir, class_name, class_code, df_val, class_list):
     '''
-    Make the label.txt files
+    Make the class_name.csv files
 
     :param folder: trai, validation or test
     :param dataset_dir: self explanatory
@@ -81,9 +81,10 @@ def get_label(folder, dataset_dir, class_name, class_code, df_val, class_list):
     :param class_list: list of the class if multiclasses is activated
     :return: None
     '''
+    tqdm.pandas()
     print("[INFO] Creating labels for {} of {}.".format(class_name, folder))
-
     image_dir = folder
+
     if class_list is not None:
         download_dir = os.path.join(dataset_dir, image_dir, class_list)
         label_dir = os.path.join(dataset_dir, folder, class_list, 'Label')
@@ -94,29 +95,7 @@ def get_label(folder, dataset_dir, class_name, class_code, df_val, class_list):
     downloaded_images_list = [f.split('.')[0] for f in os.listdir(download_dir) if f.endswith('.jpg')]
     images_label_list = list(set(downloaded_images_list))
 
-    for image in images_label_list:
-        try:
-            current_image_path = os.path.join(download_dir, image + '.jpg')
-            dataset_image = cv2.imread(current_image_path)
-            boxes = df_val[['XMin', 'XMax', 'YMin', 'YMax']][
-                (df_val.ImageID == image.split('.')[0]) & (df_val.LabelName == class_code)].values.tolist()
-            file_name = str(image.split('.')[0]) + '.txt'
-            file_path = os.path.join(label_dir, file_name)
-            if os.path.isfile(file_path):
-                f = open(file_path, 'a')
-            else:
-                f = open(file_path, 'w')
+    df_val['IN_FOLDER'] = df_val[df_val['LabelName'] == class_code]['ImageID'].progress_apply(lambda x: True if x in images_label_list else False)
+    df_val[(df_val['IN_FOLDER'] == True)][['ImageID', 'XMin', 'XMax', 'YMin', 'YMax']].to_csv(label_dir+ '/' + class_name +'.csv', index = False)
 
-            for box in boxes:
-                box[0] *= int(dataset_image.shape[1])
-                box[1] *= int(dataset_image.shape[1])
-                box[2] *= int(dataset_image.shape[0])
-                box[3] *= int(dataset_image.shape[0])
-
-                # each row in a file is XMax, YMax, XMini, YMin
-                print(class_name, box[1], box[3], box[0], box[2], file=f)
-
-        except Exception as e:
-            print(str(e))
-
-    print('[INFO] Labels creation completed.')
+    print('[INFO] Labels creation completed. Image count: {}, Labels count: {}'.format(len(images_label_list), len(df_val[df_val['IN_FOLDER'] == True])))
