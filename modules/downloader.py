ef0d883127c455c7ab1c2ec2d4fcf14cf16313da
EscVM <vittorio.mazzia@gmail.com>
2018-09-07 13:04:22 +0200

    add multithreading in downloader and editing dataset

--- modules/downloader.py
+++ modules/downloader.py
@@ -2,11 +2,11 @@ import os
 import cv2
 from tqdm import tqdm
 from modules.utils import images_options
+from multiprocessing.dummy import Pool as ThreadPool 
 
-def download(args, df_val, folder, dataset_dir, class_name, class_code, class_list=None):
+def download(args, df_val, folder, dataset_dir, class_name, class_code, class_list=None, threads = 20):
     '''
     Manage the download of the images and the label maker.
-
     :param args: argument parser.
     :param df_val: DataFrame Values
     :param folder: train, validation or test
@@ -14,6 +14,7 @@ def download(args, df_val, folder, dataset_dir, class_name, class_code, class_li
     :param class_name: self explanatory
     :param class_code: self explanatory
     :param class_list: list of the class if multiclasses is activated
+    :param threads: number of threads
     :return: None
     '''
     print('-' * 10 + class_name + '-' * 10)
@@ -26,18 +27,18 @@ def download(args, df_val, folder, dataset_dir, class_name, class_code, class_li
         class_name_list = '_'.join(class_list)
     else:
         class_name_list = class_name
-    download_img(folder, dataset_dir, class_name_list, images_list)
+    download_img(folder, dataset_dir, class_name_list, images_list, threads)
     get_label(folder, dataset_dir, class_name, class_code, df_val, class_name_list)
 
 
-def download_img(folder, dataset_dir, class_name, images_list):
+def download_img(folder, dataset_dir, class_name, images_list, threads):
     '''
     Download the images.
-
     :param folder: train, validation or test
     :param dataset_dir: self explanatory
     :param class_name: self explanatory
     :param images_list: list of the images to download
+    :param threads: number of threads
     :return: None
     '''
     print("[INFO] Found {} online images for {}.".format(len(images_list), folder))
@@ -47,14 +48,21 @@ def download_img(folder, dataset_dir, class_name, images_list):
     downloaded_images_list = [f.split('.')[0] for f in os.listdir(download_dir)]
     images_list = list(set(images_list) - set(downloaded_images_list))
 
+    pool = ThreadPool(threads)
+    
     if len(images_list) > 0:
         print("[INFO] Download of {} images in {}.".format(len(images_list), folder))
-        for image in tqdm(images_list):
+        commands = []
+        for image in images_list:
             path = image_dir + '/' + str(image) + '.jpg ' + '"' + download_dir + '"'
             command = 'aws s3 --no-sign-request --only-show-errors cp s3://open-images-dataset/' + path
-            os.system(command)
+            commands.append(command)
+        
+        list(tqdm(pool.imap(os.system, commands), total = len(commands) ))
 
         print('[INFO] Done!')
+        pool.close() 
+        pool.join() 
     else:
         print('[INFO] All images already downloaded.')
 
@@ -62,7 +70,6 @@ def download_img(folder, dataset_dir, class_name, images_list):
 def get_label(folder, dataset_dir, class_name, class_code, df_val, class_list):
     '''
     Make the label.txt files
-
     :param folder: trai, validation or test
     :param dataset_dir: self explanatory
     :param class_name: self explanatory
