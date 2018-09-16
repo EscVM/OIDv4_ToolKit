7efa6d07b59d5a36ebef724842c95e944efea873
Vittorio <vittorio.mazzia@gmail.com>
2018-09-17 01:04:05 +0200

    Update show.py
    - Add labels' names
    - Add colors for multiclass
    - Fix window dimension

--- modules/show.py
+++ modules/show.py
@@ -1,16 +1,39 @@
 import cv2
 import os
+import numpy as np
+
+class_list = []
+color_dic = dict()
+flag = 0
+window_name = 'Visualizer'
+
+def color_gen():
+    '''
+    Generate a new color. As first color generates (0, 255, 0)
+    '''
+    global flag
+  
+    if flag == 0:
+        color = (0, 255, 0)
+        flag += 1
+    else:
+        np.random.seed()
+        color = tuple(255 * np.random.rand(3))
+    return color
 
 def show(class_name, download_dir, label_dir, index):
     '''
     Show the images with the labeled boxes.
+
     :param class_name: self explanatory
     :param download_dir: folder that contains the images
     :param label_dir: folder that contains the labels
     :param index: self explanatory
     :return: None
     '''
-    cv2.namedWindow(class_name, cv2.WINDOW_NORMAL)
+ 
+    global class_list, color_dic, window_name
+
     if not os.listdir(download_dir)[index].endswith('.jpg'):
         index += 2
     img_file = os.listdir(download_dir)[index]
@@ -20,12 +43,27 @@ def show(class_name, download_dir, label_dir, index):
     file_path = os.path.join(label_dir, file_name)
     f = open(file_path, 'r')
 
-    for line in f:
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    width = 600
+    height = int((img.shape[0] * width) / img.shape[1])
+    cv2.resizeWindow(window_name, width, height)
+
+    for line in f:        
         # each row in a file is class_name, XMin, YMix, XMax, YMax
         ax = line.split(' ')
-	    # opencv top left bottom right
+	# opencv top left bottom right
+        class_name = str(ax[0])
+
+        if class_name not in class_list:
+            class_list.append(class_name)
+            color = color_gen()     
+            color_dic[class_name] = color  
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        r ,g, b = color_dic[class_name]
+        cv2.putText(img,class_name,(int(float(ax[1]))+5,int(float(ax[2]))-7), font, 0.8,(b, g, r), 2,cv2.LINE_AA)
         cv2.rectangle(img, (int(float(ax[-2])), int(float(ax[-1]))),
                       (int(float(ax[-4])),
-                       int(float(ax[-3]))), (0, 255, 0), 3)
+                       int(float(ax[-3]))), (b, g, r), 3)
 
-    cv2.imshow(class_name, img)
+    cv2.imshow(window_name, img)
