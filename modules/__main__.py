6f892091615c33c99d0e412023176e4d4dcd9301
James Adams <monocongo@gmail.com>
2019-09-01 17:58:45 -0400

    updates for using the NVIDIA Docker command instead of vanilla docker

---
+++ modules/__main__.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+from modules.parser import parser_arguments
+from modules.bounding_boxes import bounding_boxes_images
+from modules.image_level import image_level
+
+ROOT_DIR = ''
+DEFAULT_OID_DIR = os.path.join(ROOT_DIR, 'OID')
+
+
+def main():
+
+    args = parser_arguments()
+
+    if args.command == 'downloader_ill':
+        image_level(args, DEFAULT_OID_DIR)
+    else:
+        bounding_boxes_images(args, DEFAULT_OID_DIR)
+
+
+if __name__ == "__main__":
+
+    main()
